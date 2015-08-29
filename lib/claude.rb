f49c8c59ecd58279d9d4903dffc7b3cdc245a435
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-29 14:14:59 +0300

    Garden work around lib/claude.rb

--- lib/claude.rb
+++ lib/claude.rb
@@ -10,9 +10,8 @@ module Claude
   mattr_accessor :ssl_cipher
   self.ssl_cipher = 'AES-256-CBC'
 
-  # The coder is used to encode decode the keys and ivs to DB string column
+  # The coder is used to encode decode the keys and IVs to DB string column
   # friendly characters. Base64 encoding by default.
   mattr_accessor :coder
   self.coder = Coder
 end
-
